1a0d71b546d9ade6f9d8cb203f257413e40b3b35
cwilliams <coreywllms9@gmail.com>
2020-03-16 15:41:43 -0400

    just updating minimum code

--- src/main/java/games/Blizzard.java
+++ src/main/java/games/Blizzard.java
@@ -1,6 +1,7 @@
 package games;
 
 import input.ConsoleManager;
+import models.characters.Enemy;
 import models.characters.Hero;
 import models.utility.HeroWarehouse;
 import output.Display;
@@ -12,40 +13,43 @@ public class Blizzard {
     private static Display gameDisplay = new Display();
     private static ConsoleManager gameConsole = new ConsoleManager();
     private static Hero currentPlayer;
+    private static Enemy tree;
     private static Timer timer;
+    private static Integer playerHealth;
+    private static Boolean alive = false;
 
     private Blizzard() {
     }
-
 
     public static void start(HeroWarehouse characterCollection) {
         currentPlayer = characterCollection.getCharacterModelStorage().get(0);
         timer = new Timer();
 
         gameDisplay.printMessage("\nYou've entered the Teradome.");
-        gameDisplay.printMessage("Are you an Advanced, Novice, or Beginner.\nEnter 1 for Advanced: 2 for Novice: 3 for Beginner.");
+        gameDisplay.printMessage("Are you a Beginner, Novice, or Advanced Challenger.\nEnter 1 for Beginner : 2 for Novice: 3 for Advanced.");
 
         int selection = Integer.parseInt(gameConsole.playerInput());
 
         switch (selection){
             case 1:
-                gameDisplay.printMessage("You've chosen Advanced!");
+                gameDisplay.printMessage("You've chosen Beginner!");
                 break;
             case 2:
                 gameDisplay.printMessage("You've chosen Novice!");
                 break;
             case 3:
-                gameDisplay.printMessage("You've chosen Beginner!");
+                gameDisplay.printMessage("You've chosen Advanced!");
                 break;
             default:
                 gameDisplay.printMessage("PLEASE ENTER 1,2, OR 3");
                 break;
         }
     }
-//    public void checkGameHealth(){
-//        if (currentPlayer.getName().getStats().getHealth() <= 0) {
-//            gameDisplay.printMessage("Your training was tested and you chi is strong, " + currentPlayer + ".\n" + "This Game is over!");
-//            alive = false;
-//        }
-//    }
+            public void checkGameHealth(){
+                playerHealth = currentPlayer.getStats().getHealth();
+                if (playerHealth <= 0) {
+            gameDisplay.printMessage("Your training was tested and you did not survive the BLIZZARD, " + currentPlayer + ".\n" + "This Game is over!");
+            alive = false;
+        }
+    }
 }

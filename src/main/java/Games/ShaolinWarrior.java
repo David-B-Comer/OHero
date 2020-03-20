b43389ca8677c4a5703499a93a539f4e90f275f8
Mo Russ <mauriceruss302@gmail.com>
2020-03-19 20:05:22 -0400

    broke down verbose code to separate methods. start() goes to fight method but can not call pickUpScoll() after enemy death

--- src/main/java/Games/ShaolinWarrior.java
+++ src/main/java/Games/ShaolinWarrior.java
@@ -1,7 +1,5 @@
 package games;
 
-import com.sun.org.apache.xpath.internal.objects.XBoolean;
-import com.sun.org.apache.xpath.internal.objects.XBooleanStatic;
 import input.ConsoleManager;
 import models.characters.Enemy;
 import models.characters.Hero;
@@ -10,7 +8,7 @@ import models.utility.HeroWarehouse;
 import output.Display;
 
 public class ShaolinWarrior {
-    private int enemyHealth = 80;
+    private int enemyHealth = 0;
     private Enemy badGuy = new Enemy(12345L, "Bolo", "lackey");
     private double enemyMoveChoice = Math.ceil(Math.random() * 3);
     private String playerName;
@@ -21,6 +19,7 @@ public class ShaolinWarrior {
     private ConsoleManager gameConsole = new ConsoleManager();
     private Hero currentPlayer;
     private boolean inGame = true;
+    private int playerDex;
 
 
     public ShaolinWarrior() {
@@ -49,102 +48,115 @@ public class ShaolinWarrior {
         /**
          * games as on as long as player is alive
          */
-        currentPlayer = characterCollection.getCharacterModelStorage().get(0);
-        playerName = currentPlayer.getName();
-        playerHealth = currentPlayer.getStats().getHealth();
-        gameDisplay.printMessage("There has been a travesty at the Shoalin monastery. \n" +
-                "Your master Huang Zongxi has been found slain. \n" +
-                "All signs show that it was the disciples of Wang Shichong. \n" +
-                "You have traveled to Kāimíng to exact your revenge for the the honor of your master \n" +
-                "You enter the enemy's dojo. " + badGuy.getName() + " is knealing in meditation. \n" +
-                "He raises his head. You lock eyes and he rises to fight you.");
+            currentPlayer = characterCollection.getCharacterModelStorage().get(0);
+            playerName = currentPlayer.getName();
+            playerHealth = currentPlayer.getStats().getHealth();
+            alive = true;
+            inGame = true;
+            gameDisplay.printMessage("There has been a travesty at the Shoalin monastery. \n" +
+                    "Your master Huang Zongxi has been found slain. \n" +
+                    "All signs show that it was the disciples of Wang Shichong. \n" +
+                    "You have traveled to Kāimíng to exact your revenge for the the honor of your master \n" +
+                    "You enter the enemy's dojo. " + badGuy.getName() + " is knealing in meditation. \n" +
+                    "He raises his head. You lock eyes and he rises to fight you.");
+        gameDisplay.printMessage("What would you like to do? (a)punch (b)kick (c)knee strike: ");
+           fight();
 
-        alive = true;
-        while (inGame) {
+
+    }
+
+    public void fight() {
+        enemyHealth = 20;
+        alive =true;
+        while(inGame) {
 
 
-            gameDisplay.printMessage(badGuy.getName() + " has " + enemyHealth + " health remaining.");
-            gameDisplay.printMessage(playerName + " has " + playerHealth + " health remaining.");
+        gameDisplay.printMessage(badGuy.getName() + " has " + enemyHealth + " health remaining.");
+        gameDisplay.printMessage(playerName + " has " + playerHealth + " health remaining.");
 
-            gameDisplay.printMessage("What would you like to do? (a)punch (b)kick (c)knee strike: ");
-            String playerFightChoice = gameConsole.playerInput();
+        gameDisplay.printMessage("What would you like to do? (a)punch (b)kick (c)knee strike: ");
+        String playerFightChoice = gameConsole.playerInput();
 
-            double enemyChoice = Math.ceil(Math.random() * 3);
-            checkGameHealth();
-            /**
-             * switch statement takes the players choices a,b,c and
-             * compares to random generated enemy choice should
-             * create nine different occurrences of responses
-             */
+        double enemyChoice = Math.ceil(Math.random() * 3);
+        checkGameHealth();
+        /**
+         * switch statement takes the players choices a,b,c and
+         * compares to random generated enemy choice should
+         * create nine different occurrences of responses
+         */
 
-            switch (playerFightChoice) {
-                case "a":
-                    switch ((int) enemyChoice) {
-                        case 1:
-                            gameDisplay.printMessage(badGuy.getName() + " side steps your punch." + badGuy.getName() + " is faster than you thought");
-                            checkGameHealth();
-                            break;
-                        case 2:
-                            gameDisplay.printMessage(playerName + " lands a solid punch to " + badGuy.getName() + "'s ribs.");
-                            enemyHealth -= 10;
-                            checkGameHealth();
-                            break;
-                        case 3:
-                            gameDisplay.printMessage(badGuy.getName() + " sidesteps and counters with kick into " + playerName + ".");
-                            playerHealth -= 10;
-                            checkGameHealth();
-                            break;
-                    }
-                    break;
-                //enemyChoice = Math.ceil(Math.random() * 3);
-                case "b":
-                    switch ((int) enemyChoice) {
-                        case 1:
-                            gameDisplay.printMessage(badGuy.getName() + " dances by kick.");
-                            checkGameHealth();
-                            break;
-                        case 2:
-                            gameDisplay.printMessage(playerName + " your kick doubles " + badGuy.getName() + " over.");
-                            enemyHealth -= 10;
-                            checkGameHealth();
-                            break;
-                        case 3:
-                            gameDisplay.printMessage(badGuy.getName() + " jumps back from your kick. And counters with a punch to " + playerName + "'s ribs.'");
-                            playerHealth -= 10;
-                            checkGameHealth();
-                            break;
-                    }
-                    break;
-                // enemyChoice = Math.ceil(Math.random() * 3);
-                case "c":
-                    switch ((int) enemyChoice) {
-                        case 1:
-                            gameDisplay.printMessage(badGuy.getName() + " laughs at your missed knee attack.");
-                            checkGameHealth();
-                            break;
-                        case 2:
-                            gameDisplay.printMessage(playerName + " your knee doubles " + badGuy.getName() + " over.");
-                            enemyHealth -= 10;
-                            checkGameHealth();
-                            break;
-                        case 3:
-                            gameDisplay.printMessage(badGuy.getName() + " evades your knee and lands a glancing blow to your face.");
-                            playerHealth -= 10;
-                            checkGameHealth();
-                            break;
-                        default:
-                            gameDisplay.printMessage("PLEASE ENTER a, b, OR c");
-                            break;
-                    }
-                    break;
+        switch (playerFightChoice) {
+            case "a":
+                switch ((int) enemyChoice) {
+                    case 1:
+                        gameDisplay.printMessage(badGuy.getName() + " side steps your punch." + badGuy.getName() + " is faster than you thought");
+                        checkGameHealth();
+                        break;
+                    case 2:
+                        gameDisplay.printMessage(playerName + " lands a solid punch to " + badGuy.getName() + "'s ribs.");
+                        enemyHealth -= 10;
+                        checkGameHealth();
+                        break;
+                    case 3:
+                        gameDisplay.printMessage(badGuy.getName() + " sidesteps and counters with kick into " + playerName + ".");
+                        playerHealth -= 10;
+                        checkGameHealth();
+                        break;
+                }
+                break;
+            //enemyChoice = Math.ceil(Math.random() * 3);
+            case "b":
+                switch ((int) enemyChoice) {
+                    case 1:
+                        gameDisplay.printMessage(badGuy.getName() + " dances by kick.");
+                        checkGameHealth();
+                        break;
+                    case 2:
+                        gameDisplay.printMessage(playerName + " your kick doubles " + badGuy.getName() + " over.");
+                        enemyHealth -= 10;
+                        checkGameHealth();
+                        break;
+                    case 3:
+                        gameDisplay.printMessage(badGuy.getName() + " jumps back from your kick. And counters with a punch to " + playerName + "'s ribs.'");
+                        playerHealth -= 10;
+                        checkGameHealth();
+                        break;
+                }
+                break;
+            // enemyChoice = Math.ceil(Math.random() * 3);
+            case "c":
+                switch ((int) enemyChoice) {
+                    case 1:
+                        gameDisplay.printMessage(badGuy.getName() + " laughs at your missed knee attack.");
+                        checkGameHealth();
+                        break;
+                    case 2:
+                        gameDisplay.printMessage(playerName + " your knee doubles " + badGuy.getName() + " over.");
+                        enemyHealth -= 10;
+                        checkGameHealth();
+                        break;
+                    case 3:
+                        gameDisplay.printMessage(badGuy.getName() + " evades your knee and lands a glancing blow to your face.");
+                        playerHealth -= 10;
+                        checkGameHealth();
+                        break;
+                    default:
+                        gameDisplay.printMessage("PLEASE ENTER a, b, OR c");
+                        break;
+                }
+                break;
 
-            }
         }
-
+    }
+        if(enemyHealth <= 0){
+            pickUpScroll();
+        }
+}
         /**
          * If player survives first fight
          * pick up item method
          */
+        public void pickUpScroll() {
         while (inGame) {
             gameDisplay.printMessage("There is a scroll on the ground. Do you want; a) to pick it up or b) go to the next room");
 
@@ -157,7 +169,7 @@ public class ShaolinWarrior {
                 gameDisplay.printMessage("You rush into a room full of soldiers. This choice might have been rash!");
             break;
         }
-    }
+        }
 
     /**
      * made to stop game upon player death
